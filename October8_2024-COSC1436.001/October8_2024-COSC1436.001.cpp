cc64b074f6df844b334f48c0b6de45eca20655c1
Work <Work@MSI>
2024-10-08 10:22:11 -0500

    Add project files.

---
+++ October8_2024-COSC1436.001/October8_2024-COSC1436.001.cpp
@@ -0,0 +1,59 @@
+
+#include <iostream>
+using std::cout; 
+
+int main()
+{
+	//while (somethingIsTrue)
+	//do some stuff 
+	//how long does it take a C++ program ON MY MACHINE to count to 1 billion? 
+	//int counter = 1; 
+
+	//while (counter < 1'000'000'000)
+	//{
+	//	//counter = counter + 1; 
+	//	counter++;  //this is "shorthand" 
+
+	//	if (counter % 500'000 == 0)
+	//	{
+	//		cout << counter << "\n";
+	//	}
+	//}
+
+	//cout << "....Done!\n What a fast machine!\n";
+	//Suppose you have 100 buckaroos in your bank account and go to the grocery store ...
+	//double accountBalance = 100.00; 
+
+	//double amountSpentSoFar = 0.00; 
+
+	//do
+	//{
+
+	//} while (); 
+	//while (true)
+	//{
+
+	//}
+	//while (amountSpentSoFar <= accountBalance)
+	//{
+	//	double costOfCurrentItem; 
+	//	cout << "How much does the current item that you are going to buy cost? \n";
+	//	std::cin >> costOfCurrentItem; 
+
+
+	//	//amountSpentSoFar = amountSpentSoFar + costOfCurrentItem; 
+	//	//if (amountSpentSoFar > accountBalance)
+	//	//{
+	//	//	break; 
+	//	//}
+	//}
+
+	//cout << "You have spent all of your money - don't overdraw\n";
+	//for(int i = 0; i < 10; i++)
+	char choice = '0';
+	while (choice != 'q')
+	{
+		cout << "enter your choice:\n";
+		std::cin >> choice; 
+	}
+}
